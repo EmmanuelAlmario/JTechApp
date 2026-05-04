c9f3ae1bba5de252643be77cda1111625dcbf2b6
Emmanuel <emanuelalmarionovoa@gmail.com>
2026-05-04 02:15:05 -0500

    entities created, there are some bugs to fix.

--- src/main/java/com/jtech/JtechApp/entity/Subcategoria.java
+++ src/main/java/com/jtech/JtechApp/entity/Subcategoria.java
@@ -1,4 +1,27 @@
 package com.jtech.JtechApp.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "subcategorias")
 public class Subcategoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @ManyToOne()
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
 }
